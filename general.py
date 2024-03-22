864b904e9e893506cea34e0e91c5ba6b47fab9df
jinxw <jinxw@live.it>
2024-03-22 18:30:30 +0100

    set up utilities

--- general.py
+++ general.py
@@ -1,7 +1,40 @@
 import os
 
+# create a folder for each project that would be crawled
 def create_project_dir(directory):
     if not os.path.exists(directory):
         print('Crating project ' + directory)
         os.makedirs(directory)
 
+def create_data_file(directory, url):
+    queue = directory + os.path.sep + 'queue.txt'
+    crawled = directory + os.path.sep + 'crawled.txt'
+    if not os.path.exists(queue):
+        create_file(directory, url)
+    if not os.path.exists(crawled):
+        create_file(directory, '')
+
+def create_file(directory, content):
+    f = open(directory, 'w')
+    f.write(content)
+    f.close
+
+def append_to_file(path, data):
+    with open(path, 'a') as file:
+        file.write(data + os.linesep)
+
+def delete_file_content(path):
+    with open(path, 'w'):
+        pass
+
+def file_to_set(filename):
+    results = set()
+    with open(filename, 'rt') as f:
+        for line in f:
+            results.add(line.replace(os.linesep, ''))
+    return results
+
+def set_to_file(links, file):
+    delete_file_content(file)
+    for link in sorted(links):
+        append_to_file(file, link)
---
+++ readme.md
@@ -0,0 +1,3 @@
+# About
+This project is a hands-on work from the youtube video
+https://www.youtube.com/watch?v=z_vIWoTZm2E&list=PL6gx4Cwl9DGA8Vys-f48mAH9OKSUyav0q
